4892c15d6b55d4cb26289ab15344c44f770ef85f
iamacoder12 <109200031+iamacoder12@users.noreply.github.com>
2023-06-20 21:58:36 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
-Webcam.set({
-
-    width:350,
-    height:300,
-    image_format : 'png' ,
-    png_quality:90
-});
+Webcam.attach( '#camera' );
 
 camera = document.getElementById("camera");
-
-Webcam.attach('#camera')
-
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
 
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id = "captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
     });
 }
 
-console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7b1ogt4AE/model.json',modelLoaded);
+  console.log('ml5 version:', ml5.version);
+  
+  // Initialize the Image Classifier method with MobileNet
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7b1ogt4AE.json',modelLoaded);
 
-function modelLoaded() {
-    console.log("modelLoaded")
-
-}
-
-
-function check()
-{
-    img = document.getElementById('captured_image');
+  // When the model is loaded
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
     classifier.classify(img, gotResult);
-}
+  }
 
+
+// A function to run when we get any errors and the results
 function gotResult(error, results) {
-    if(error){
-        console.error(error);
-    } else{
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence
-    }
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
 }
